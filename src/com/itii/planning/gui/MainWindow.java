1ee78f58c92c0fa53ebf86e265c7f24f1fb6e8fb
Unknown <theodore.bories@free.fr>
2018-04-28 12:55:35 +0200

    GUI avec WindowBuilder

--- src/com/itii/planning/gui/MainWindow.java
+++ src/com/itii/planning/gui/MainWindow.java
@@ -1,22 +1,25 @@
 package com.itii.planning.gui;
 
-import java.awt.BorderLayout;
-import java.awt.Color;
-import java.awt.Dimension;
-import java.awt.GridLayout;
 
+import java.awt.Dimension;
 import javax.swing.JFrame;
 import javax.swing.JMenuBar;
 import javax.swing.JMenu;
 import javax.swing.JMenuItem;
+import javax.swing.BoxLayout;
+import javax.swing.JComboBox;
+import java.awt.Component;
 import javax.swing.JPanel;
 import javax.swing.JScrollPane;
-import javax.swing.JSplitPane;
-import javax.swing.JTextArea;
+import java.awt.FlowLayout;
+import java.awt.ScrollPane;
 import javax.swing.JTextField;
-import javax.swing.BorderFactory;
-import javax.swing.BoxLayout;
+import javax.swing.JSeparator;
+import javax.swing.JEditorPane;
+import javax.swing.SpringLayout;
 import javax.swing.JButton;
+import java.awt.BorderLayout;
+import javax.swing.SwingConstants;
 
 /**
  * 
@@ -25,6 +28,7 @@ import javax.swing.JButton;
 public class MainWindow extends JFrame
 {
     private static MainWindow instance = null;
+    private JTextField textField;
 
     public static MainWindow getInstance()
     {
@@ -38,7 +42,7 @@ public class MainWindow extends JFrame
     // TODO JavaDoc
     private MainWindow()
     {
-        CreatePanels();//TOOD Remove this
+        CreatePanels();
         CreateMenuBar();// la creation du menu bar doit se faire avant le initialize
         initialize();
     }
@@ -53,12 +57,15 @@ public class MainWindow extends JFrame
         this.validate();
         this.repaint();
         this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        
+        
+
     }
 
     // TODO javadoc
     private void CreateMenuBar()
     {
-        setTitle("Menu Example");
+        setTitle("Menu");
 
         // Creates a menubar for a JFrame
         JMenuBar menuBar = new JMenuBar();
@@ -105,64 +112,44 @@ public class MainWindow extends JFrame
         ViewMenu.add(WeeksAction);
         ViewMenu.add(MonthAction);
 
+
     }
 
     // TODO Javadoc
     private void CreatePanels()
     {
-        // these are the components we need.
-
-        JPanel LeftPanel; // container panel for the bottom
-        JPanel RightPanel; // container panel for the top
-
-        JButton button;
-
-        RightPanel = new JPanel(); // our top component
-        LeftPanel = new JPanel(); // our bottom component
-
-        button = new JButton("C'est un bouton"); // and a button at the right, to send the
-
-        setPreferredSize(new Dimension(200, 200)); // let's open the window with
-        getContentPane().setLayout(new GridLayout()); // the default GridLayout
-
-        RightPanel.setSize(200, 40);
-        RightPanel.setLayout(new GridLayout(5, 1)); // BoxLayout.Y_AXIS
-        RightPanel.setBorder(BorderFactory.createLineBorder(Color.black));
+        getContentPane().setLayout(new BoxLayout(getContentPane(), BoxLayout.X_AXIS));
         
+        JPanel panel = new JPanel();
+        panel.setAlignmentY(Component.TOP_ALIGNMENT);
+        getContentPane().add(panel);
+        panel.setLayout(null);
         
-        LeftPanel.setLayout(new GridLayout(1, 1)); // BoxLayout.Y_AXIS
-        LeftPanel.setBorder(BorderFactory.createLineBorder(Color.black));
+        textField = new JTextField();
+        textField.setBounds(20, 5, 358, 20);
+        panel.add(textField);
+        textField.setColumns(44);
         
+        JComboBox comboBox = new JComboBox();
+        comboBox.setBounds(400, 5, 28, 20);
+        panel.add(comboBox);
         
-        RightPanel.add(button);
+        JScrollPane scrollPane = new JScrollPane();
+        scrollPane.setBounds(555, 62, -533, 367);
+        panel.add(scrollPane);
         
-        //RightPanel.setBackground(Color.ORANGE);   
-        //LeftPanel.setBackground(Color.RED);
+        JPanel panel_1 = new JPanel();
+        panel_1.setAlignmentY(Component.TOP_ALIGNMENT);
+        getContentPane().add(panel_1);
+        panel_1.setLayout(new BoxLayout(panel_1, BoxLayout.X_AXIS));
         
-        getContentPane().add(LeftPanel);   
-        getContentPane().add(RightPanel); 
+        JButton btnNewButton = new JButton("New button");
+        panel_1.add(btnNewButton);
         
-        /*setSize(500,400);
-        setVisible(true);
-
-    //declaration container
-        getContentPane();
-    
-    
-        JPanel menu =new JPanel();
-        JPanel MessageList =new JPanel();
-        JPanel about=new JPanel();
-    
-        menu.setBackground(Color.blue);
-        MessageList.setBackground(Color.cyan);
-        about.setBackground(Color.cyan);
+        JButton btnNewButton_1 = new JButton("New button");
+        panel_1.add(btnNewButton_1);
         
-        //--------Button---------------------
-        JButton button1=new JButton("button1");
-        JButton button2=new JButton("Button2");
-    
-        menu.add(button1);
-        menu.add(button2);*/
-      
+        JButton btnNewButton_2 = new JButton("New button");
+        panel_1.add(btnNewButton_2);
     }
 }
