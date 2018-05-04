bffd59a40abbfba60e0174685915b83bc59a5aee
Unknown <lea_cruiziat@yahoo.fr>
2018-05-04 16:49:20 +0200

    Second Window
    Second Window avec suppression de calendrier car insatisfaisant

--- src/com/itii/planning/gui/SecondWindow.java
+++ src/com/itii/planning/gui/SecondWindow.java
@@ -1,20 +1,20 @@
 package com.itii.planning.gui;
-import com.itii.planning.gui.CalendarPanel;
-
-
-import javax.swing.JFrame;
 import java.awt.Component;
-import javax.swing.JPanel;
 import java.awt.Font;
 import java.awt.GridLayout;
 import java.awt.SystemColor;
 
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
 import javax.swing.JTextField;
 import javax.swing.JTextPane;
 
+//import de.wannawork.jcalendar.*;
+
 /**
  * 
- * @author Léa - Théodore - Groupe E TODO Fill javadoc
+ * @author Lï¿½a - Thï¿½odore - Groupe E TODO Fill javadoc
  */
 public class SecondWindow extends JFrame
 {
@@ -23,6 +23,7 @@ public class SecondWindow extends JFrame
 
     public static SecondWindow getInstance()
     {
+
         if (instance == null)
         {
             instance = new SecondWindow();
@@ -33,7 +34,7 @@ public class SecondWindow extends JFrame
     private SecondWindow()
     {
         createPanel();
-        createCalendar();
+        //createCalendar();
         initialize();
   
     }
@@ -44,7 +45,7 @@ public class SecondWindow extends JFrame
 
         this.setSize(1000, 500);
         this.setLocationRelativeTo(null);
-        this.setTitle("Nouvelle Tâche");
+        this.setTitle("Nouvelle Tï¿½che");
         this.setVisible(true);
         this.validate();
         this.repaint();
@@ -61,27 +62,35 @@ public class SecondWindow extends JFrame
         JTextPane label=new JTextPane();
         label.setBackground(SystemColor.menu);
         label.setFont(new Font("Sylfaen", Font.PLAIN,22));
-        label.setText("Nouvelle Tâche : ");
+        label.setText("Nouvelle Tï¿½che : ");
         label.setBounds(5, 50, 200, 40);
         panel_Left.add(label);
         textField = new JTextField();
         textField.setFont(new Font("Sylfaen", Font.PLAIN,22));
-        textField.setBounds(200, 50, 400, 40);
+        textField.setBounds(200, 50, 750, 40);
         panel_Left.add(textField);
         
         JTextPane label1=new JTextPane();
         label1.setBackground(SystemColor.menu);
         label1.setFont(new Font("Sylfaen", Font.PLAIN,22));
-        label1.setText("Date dûe : ");
+        label1.setText("Date dï¿½e : ");
         label1.setBounds(5, 100, 200, 40);
         panel_Left.add(label1);
+        JTextField textField1 = new JTextField();
+        textField1.setFont(new Font("Sylfaen", Font.PLAIN,22));
+        textField1.setBounds(200, 100, 400, 40);
+        panel_Left.add(textField1);
         
         JTextPane label2=new JTextPane();
         label2.setBackground(SystemColor.menu);
         label2.setFont(new Font("Sylfaen", Font.PLAIN,22));
-        label2.setText("Détails : ");
+        label2.setText("Dï¿½tails : ");
         label2.setBounds(5, 150, 200, 40);
         panel_Left.add(label2);
+        JTextField textField2 = new JTextField();
+        textField2.setFont(new Font("Sylfaen", Font.PLAIN,22));
+        textField2.setBounds(200, 150, 400, 350);
+        panel_Left.add(textField2);
        
         
         panel_Left.setAlignmentY(Component.TOP_ALIGNMENT);
@@ -91,19 +100,13 @@ public class SecondWindow extends JFrame
         textField.setFont(new Font("Sylfaen", Font.PLAIN,22));
         textField.setBounds(200, 50, 400, 40);
         panel_Left.add(textField);
+        panel_Left.revalidate();
     }
 
 
-    private void createCalendar(){
+   /* private void createCalendar(){
+        JCalendarPanel calendrier=new JCalendarPanel();
+      
+    }*/
     
-        CalendarPanel calendar= new CalendarPanel();
-       // calendar.setBounds(500, 50, 20, 40);
-        getContentPane().setLayout(new GridLayout(1,1));
-        JPanel panel_Right = new JPanel();
-        panel_Right.setAlignmentY(Component.TOP_ALIGNMENT);
-        getContentPane().add(panel_Right);
-        panel_Right.add(calendar);
-        
-        
-    }
 }
